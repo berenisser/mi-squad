307f1c2f33c8ea32f5adb7701fe22247d141f6d4
berenisser <berenisser@gmail.com>
2017-05-05 01:57:14 -0300

    Lista de hobbies aparece

--- assets/main.js
+++ assets/main.js
@@ -1,7 +1,7 @@
-function MiembroSquad(nombre, edad, hobbie){
+function MiembroSquad(nombre, edad, hobbies){
 	this.nombre = nombre;
 	this.edad = edad;
-	this.hobbie = hobbie;
+	this.hobbies = hobbies;
 }
 
 var squad = [];
@@ -14,12 +14,21 @@ var Clau = new MiembroSquad("Claudia Rivera", 33, ["Gatos","Patinar","Comer dulc
 squad.push(Clau);
 var Susana = new MiembroSquad("Susuna Arce", 28, ["Ficcion","Efusividad","Emprender"]);
 squad.push(Susana);
-var Rata = new MiembroSquad("Chinchilla Psico", 24, ["Dibujar","leer","aprender"]);
+var Rata = new MiembroSquad("Chinchilla Psico", 24, ["Dibujar","leer","Aprender"]);
 squad.push(Rata);
 var Nico = new MiembroSquad("Nicole Aliste", 32, ["Juegos de video","Cosas Kawai","Alicia"]);
 squad.push(Nico);
 
 var impresion = document.getElementById('imprimir');
 squad.forEach(function(el){
-	impresion.innerHTML += "<div>"+ "<b>Nombre: </b>"+el.nombre +"<br>" + "<b>Edad: </b>"+el.edad+"<br><br>"+"</div>";
+
+	impresion.innerHTML += "<div>"+ "<br><b>Nombre: </b>"+el.nombre +"<br>" + "<b>Edad: </b>"+el.edad+" años<br>"+ "<b>Hobbies: </b>" +"</div>";
+
+	var listaHobbies = el.hobbies.forEach(function(e){
+		return (impresion.innerHTML += "<li>"+e+"</li>");
+	});
 });
+
+/*No encontré manera de incluir la variable listaHobbies dentro de la etiqueta <ul> 
+en la linea 25 sin que aparezca como undefined, al no poder hacer eso no pude hacer
+que la lista de hobbies aparezca dentro del <div> de cada miembro del squad */
